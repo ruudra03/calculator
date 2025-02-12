947d1fcc46299ff9d309828955a394c5df08fd92
Rudra Patel <work.rudra@aol.com>
2025-02-12 20:28:07 +0000

    Add calculator display and keypad inside HTML
    Provide basic CSS styles for the modified HTML.
    Use JS to set starting calculator display value.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Log
 console.log("JavaScript loaded.");
+
+// Script variables
+let displayValue = "0"; // default display value
+
+// Select calculator element
+const calculator = document.querySelector("#calculator");
+
+// Select calculator display
+const display = calculator.querySelector("#display");
+display.textContent = displayValue; // set display value
