c25c2620a3dd836fcfd847bf37e93943b9b36d0e
Rudra Patel <work.rudra@aol.com>
2025-02-12 20:37:23 +0000

    Modify variable names inside index.js, and also CSS styles

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 console.log("JavaScript loaded.");
 
 // Script variables
-let displayValue = "0"; // default display value
+let display = "0"; // default display value
 
 // Select calculator element
-const calculator = document.querySelector("#calculator");
+const CALCULATOR = document.querySelector("#calculator");
 
 // Select calculator display
-const display = calculator.querySelector("#display");
-display.textContent = displayValue; // set display value
+const DISPLAY = CALCULATOR.querySelector("#display");
+DISPLAY.textContent = display; // set display value
