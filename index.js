6659eabff20ce4088d3ae11df638a23f0146a706
Rudra Patel <work.rudra@aol.com>
2025-02-14 19:24:49 +0000

    Write basic operator JS functions and modify HTML
    Add basic add(), substract(), multiply(), and divide() functions.
    Edit class names of add and substract element in HTML from plus and minus to follow the naming convention.

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ const CALCULATOR = document.querySelector("#calculator");
 // Select calculator display
 const DISPLAY = CALCULATOR.querySelector("#display");
 DISPLAY.textContent = display; // set display value
+
+// Calculator operator functions
+
+// ADD
+function add(firstOperand, secondOperand) {
+  let result = firstOperand + secondOperand;
+  return result;
+}
+
+// SUBSTRACT
+function substract(firstOperand, secondOperand) {
+  let result = firstOperand - secondOperand;
+  return result;
+}
+
+// MULTIPLY
+function multiply(firstOperand, secondOperand) {
+  let result = firstOperand * secondOperand;
+  return result;
+}
+
+// DIVIDE
+function divide(firstOperand, secondOperand) {
+  let result = firstOperand / secondOperand;
+  return result;
+}
